e188ed0cb52f0276ab3ab0cac474d38027b52c3b
michelpinto <michabbe9@gmail.com>
2023-01-30 11:18:37 +0200

    setting up server

--- server.ts
+++ server.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import connectDb from './api/config/db';
+
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 6000;
+connectDb();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World, my name is Doctor');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/users', require('./api/routes/userRoutes'));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
