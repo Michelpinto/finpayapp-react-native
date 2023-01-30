d69c6e85fc19e7de55a8d10ce5c4c334a2efd0c8
michelpinto <michabbe9@gmail.com>
2023-01-30 11:19:02 +0200

    User logic

---
+++ api/controllers/userController.ts
@@ -0,0 +1,72 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel';
+import { Request, Response } from 'express';
+
+const generateToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET!, {
+    expiresIn: '30d',
+  });
+};
+
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill in all fields');
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+const getMe = asyncHandler(async (req: Request | any, res: Response) => {
+  res.status(200).json(req.user);
+});
+
+export { registerUser, loginUser, getMe };
---
+++ api/routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import express, { Express, Request, Response, Router } from 'express';
+import { registerUser, loginUser } from '../controllers/userController';
+
+const router = express.Router();
+
+router.post('/', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
