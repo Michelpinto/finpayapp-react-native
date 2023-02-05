02f060cc190bec2c69ead3b18c61e5fcc30b9684
michelpinto <michabbe9@gmail.com>
2023-02-05 18:12:16 +0200

    screens

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface Props {
   navigation: any;
 }
 
 const Home: React.FC<Props> = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const { data } = await axios.post('http://localhost:6000/users/login', {
+        email,
+        password,
+      });
+
+      const user = JSON.stringify({
+        email: data?.email,
+        id: data?._id,
+        name: data?.name,
+        token: data?.token,
+      });
+
+      await AsyncStorage.setItem('@finpayApp:user', user);
+
+      navigation.navigate('Details', {
+        email: data?.email,
+        id: data?._id,
+        name: data?.name,
+      });
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello world! This is home page</Text>
-
-      <Button
-        title='Go to Details'
-        onPress={() => navigation.navigate('Details')}
+      <TextInput
+        onChangeText={(email) => setEmail(email)}
+        value={email}
+        style={styles.input}
+        placeholder='Email'
+        autoCapitalize='none'
       />
+      <TextInput
+        onChangeText={(password) => setPassword(password)}
+        value={password}
+        secureTextEntry={true}
+        style={styles.input}
+        placeholder='Password'
+      />
+
+      <Button title='Login' onPress={handleSubmit} />
     </View>
   );
 };
@@ -26,5 +69,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    width: 200,
   },
 });
