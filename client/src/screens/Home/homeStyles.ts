ecef3865b158fdd999dc31c57b6c0618f9be04ec
michelpinto <michabbe9@gmail.com>
2023-02-11 08:57:54 +0200

    styles and image

---
+++ client/src/screens/Home/homeStyles.ts
@@ -0,0 +1,54 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('screen');
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingVertical: 120,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 40,
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  welcomeText: {
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  balanceText: {
+    fontSize: 25,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+  balance: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  Div: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logoutWrapper: {
+    width: width - 32,
+    height: 52,
+    marginBottom: 32,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  logout: {
+    borderWidth: 2,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 42,
+    borderRadius: 10,
+    backgroundColor: '#1d1d1d',
+    color: '#fff',
+  },
+});
