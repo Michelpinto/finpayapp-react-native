550d3c17cff44a128400ed492de106c376a79f61
michelpinto <michabbe9@gmail.com>
2023-02-23 10:08:16 +0200

    Movements UI

---
+++ client/src/screens/Movements/MovementsUI.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FlatList, Image, Text, View } from 'react-native';
+import { styles } from './movStyles';
+
+interface Props {
+  movements: any[];
+}
+
+const MovementsUI: React.FC<Props> = ({ movements }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.img}
+        source={require('../../../assets/movementsAsset.png')}
+      />
+      <Text style={styles.text}>Past transactions</Text>
+      {movements.map((movement) => (
+        <View style={[styles.itemContainer, styles.shadowProp]}>
+          <Text
+            style={{
+              marginBottom: 5,
+              color: movement.balance > 0 ? 'green' : 'red',
+            }}
+          >
+            {movement.balance > 0
+              ? `Received $${movement.balance}.00`
+              : `Sent $${movement.balance}.00`}
+          </Text>
+          <Text style={{ color: 'grey' }}>{movement.date.split('T')[0]}</Text>
+        </View>
+      ))}
+      {/* <FlatList
+        data={movements}
+        style={styles.list}
+        renderItem={({ item }) => (
+         
+        )}
+      /> */}
+    </View>
+  );
+};
+
+export default MovementsUI;
